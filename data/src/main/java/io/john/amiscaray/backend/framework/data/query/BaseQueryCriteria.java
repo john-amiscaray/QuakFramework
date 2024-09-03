6485d727a4f69444f968ab6b1369b69dc8fa5473
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-09-03 19:31:25 -0400

    Add dedicated Functional Interface for QueryCriteriaProvider

--- data/src/main/java/io/john/amiscaray/backend/framework/data/query/BaseQueryCriteria.java
+++ data/src/main/java/io/john/amiscaray/backend/framework/data/query/BaseQueryCriteria.java
@@ -7,58 +7,56 @@ import io.john.amiscaray.backend.framework.data.query.string.ValueLike;
 import io.john.amiscaray.backend.framework.data.query.string.ValueStartsWith;
 import lombok.AllArgsConstructor;
 
-import java.util.function.Function;
-
 @AllArgsConstructor
 public abstract class BaseQueryCriteria implements QueryCriteria{
 
     protected final String fieldName;
 
-    public static BaseQueryCriteria valueOfField(String fieldName, Function<String, BaseQueryCriteria> matches) {
-        return matches.apply(fieldName);
+    public static BaseQueryCriteria valueOfField(String fieldName, QueryCriteriaProvider queryCriteriaProvider) {
+        return queryCriteriaProvider.provideQueryCriteriaGivenFieldName(fieldName);
     }
 
-    public static Function<String, BaseQueryCriteria> is(Object value) {
+    public static QueryCriteriaProvider is(Object value) {
         return fieldName -> new ValueIs(fieldName, value);
     }
 
-    public static Function<String, BaseQueryCriteria> isOneOf(Object... values) {
+    public static QueryCriteriaProvider isOneOf(Object... values) {
         return fieldName -> new ValueIsOneOf(fieldName, values);
     }
 
-    public static Function<String, BaseQueryCriteria> isBetween(Number min, Number max) {
+    public static QueryCriteriaProvider isBetween(Number min, Number max) {
         return fieldName -> new ValueBetween(fieldName, min, max);
     }
 
-    public static Function<String, BaseQueryCriteria> isGreaterThan(Number value) {
+    public static QueryCriteriaProvider isGreaterThan(Number value) {
         return fieldName -> new ValueGreaterThan(fieldName, value);
     }
 
-    public static Function<String, BaseQueryCriteria> isGreaterThanOrEqualTo(Number value) {
+    public static QueryCriteriaProvider isGreaterThanOrEqualTo(Number value) {
         return fieldName -> new ValueGreaterThanOrEqualTo(fieldName, value);
     }
 
-    public static Function<String, BaseQueryCriteria> isLessThan(Number value) {
+    public static QueryCriteriaProvider isLessThan(Number value) {
         return fieldName -> new ValueLessThan(fieldName, value);
     }
 
-    public static Function<String, BaseQueryCriteria> isLessThanOrEqualTo(Number value) {
+    public static QueryCriteriaProvider isLessThanOrEqualTo(Number value) {
         return fieldName -> new ValueLessThanOrEqualTo(fieldName, value);
     }
 
-    public static Function<String, BaseQueryCriteria> contains(String substring) {
+    public static QueryCriteriaProvider contains(String substring) {
         return fieldName -> new ValueContaining(fieldName, substring);
     }
 
-    public static Function<String, BaseQueryCriteria> endsWith(String suffix) {
+    public static QueryCriteriaProvider endsWith(String suffix) {
         return fieldName -> new ValueEndsWith(fieldName, suffix);
     }
 
-    public static Function<String, BaseQueryCriteria> startsWith(String prefix) {
+    public static QueryCriteriaProvider startsWith(String prefix) {
         return fieldName -> new ValueStartsWith(fieldName, prefix);
     }
 
-    public static Function<String, BaseQueryCriteria> isLike(String regex) {
+    public static QueryCriteriaProvider isLike(String regex) {
         return fieldName -> new ValueLike(fieldName, regex);
     }
 
