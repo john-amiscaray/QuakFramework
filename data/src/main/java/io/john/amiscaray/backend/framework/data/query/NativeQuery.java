00f7acdbd37c4fcb0b29928ced8b0eeab16e469c
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-09-13 10:49:57 -0400

    Add test coverage, update NativeQuery to have a constructor accepting only the hql

--- data/src/main/java/io/john/amiscaray/backend/framework/data/query/NativeQuery.java
+++ data/src/main/java/io/john/amiscaray/backend/framework/data/query/NativeQuery.java
@@ -3,4 +3,9 @@ package io.john.amiscaray.backend.framework.data.query;
 import java.util.Map;
 
 public record NativeQuery(String hql, Map<String, String> params) {
+
+    public NativeQuery(String hql) {
+        this(hql, Map.of());
+    }
+
 }
