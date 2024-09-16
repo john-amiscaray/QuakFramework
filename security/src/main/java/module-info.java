f201949cb6ee06ec0a611f3290e61eb10ec2a5a4
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-09-16 08:34:36 -0400

    make security module export Principal

--- security/src/main/java/module-info.java
+++ security/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module backend.framework.security {
     exports io.john.amiscaray.backend.framework.security.auth;
     exports io.john.amiscaray.backend.framework.security.auth.credentials;
     exports io.john.amiscaray.backend.framework.security.auth.exception;
+    exports io.john.amiscaray.backend.framework.security.auth.principal;
     requires static lombok;
     requires jakarta.servlet;
     requires backend.framework.core;
