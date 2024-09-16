3c279c77f07b14958d65446bdd10c8510e940239
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-09-16 11:07:26 -0400

    Add constants for dependency names

--- security/src/main/java/io/john/amiscaray/backend/framework/security/di/SecurityDependencyIDs.java
+++ security/src/main/java/io/john/amiscaray/backend/framework/security/di/SecurityDependencyIDs.java
@@ -7,8 +7,12 @@ import jakarta.servlet.Filter;
 
 public class SecurityDependencyIDs {
 
-    public static final DependencyID<SecurityConfig> SECURITY_CONFIG_DEPENDENCY = new DependencyID<>("applicationSecurityConfig", SecurityConfig.class);
-    public static final DependencyID<Filter> SECURITY_FILTER_DEPENDENCY = new DependencyID<>("applicationSecurityFilter", Filter.class);
-    public static final DependencyID<Authenticator> AUTHENTICATOR_DEPENDENCY = new DependencyID<>("applicationAuthenticator", Authenticator.class);
+    public static final String SECURITY_CONFIG_DEPENDENCY_NAME = "applicationSecurityConfig";
+    public static final String SECURITY_FILTER_DEPENDENCY_NAME = "applicationSecurityFilter";
+    public static final String AUTHENTICATOR_DEPENDENCY_NAME = "applicationAuthenticator";
+
+    public static final DependencyID<SecurityConfig> SECURITY_CONFIG_DEPENDENCY = new DependencyID<>(SECURITY_CONFIG_DEPENDENCY_NAME, SecurityConfig.class);
+    public static final DependencyID<Filter> SECURITY_FILTER_DEPENDENCY = new DependencyID<>(SECURITY_FILTER_DEPENDENCY_NAME, Filter.class);
+    public static final DependencyID<Authenticator> AUTHENTICATOR_DEPENDENCY = new DependencyID<>(AUTHENTICATOR_DEPENDENCY_NAME, Authenticator.class);
 
 }
