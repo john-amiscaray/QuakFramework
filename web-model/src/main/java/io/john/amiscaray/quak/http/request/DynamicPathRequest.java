8a767e4f9a02cf73559d4cb545769a458a8e1ab0
John Amiscaray <john_ernest_amiscaray@outlook.com>
2024-10-15 12:22:50 -0400

    Document quak.web-model

--- web-model/src/main/java/io/john/amiscaray/quak/http/request/DynamicPathRequest.java
+++ web-model/src/main/java/io/john/amiscaray/quak/http/request/DynamicPathRequest.java
@@ -3,6 +3,16 @@ package io.john.amiscaray.quak.http.request;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A request for a path with path variables.
+ * @param headers The headers as a map.
+ * @param queryParams The query parameters as a map.
+ * @param method The request method.
+ * @param pathVariables The path variables as a map.
+ * @param body The request body.
+ * @param attributes The attributes for the request (for server-side use).
+ * @param <T> The type of the request body.
+ */
 public record DynamicPathRequest<T>(Map<String, String> headers,
                                        Map<String, String> queryParams,
                                        RequestMethod method,
