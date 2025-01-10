ed4c79a8093225d7040e881c678576d6b53f957a
John Amiscaray <john_ernest_amiscaray@outlook.com>
2025-01-10 14:59:15 -0500

    Clean imports

--- tools/QuakCLI/src/main/java/io/john/amiscaray/quak/cli/generator/ProjectGenerator.java
+++ tools/QuakCLI/src/main/java/io/john/amiscaray/quak/cli/generator/ProjectGenerator.java
@@ -4,14 +4,11 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.terminal.Terminal;
 import io.john.amiscaray.quak.cli.cfg.ProjectConfig;
 import org.apache.commons.io.FileUtils;
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
